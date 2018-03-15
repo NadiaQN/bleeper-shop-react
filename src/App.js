3b97f9b9d9fbcc59e366c9a897c8c39a9d15b53f
Kdangelo <kdangelof@gmail.com>
2018-03-15 18:35:34 -0300

    data resultados

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
+        <Navbar onResults={this._handleResults}/>
         <CartShop/>
         <ProductList/>
         <Carousel/>
--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,41 +1,45 @@
 // Aquí se mostraran los productos
 import React, { Component } from 'react';
+import Navbar from '../NavBar';
 import {Grid, Row, Col, Thumbnail, Button} from "react-bootstrap";
 import "./style.css"
 
 
 class ProductList extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            products: []
-        }
+    this.state = {
+      data: []
     }
+  }
 
+  // _handleResults = (data) => {
+  //   this.setState({ data })
+  // }
 
-    render() {
-        return (
-            <Grid>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-                            <h3>Nombre: </h3>
-                            <p>Precio: </p>
-                            <p>Condicion: </p>
-                            <p>Disponibles: </p>
-                            <p>Vendidos: </p>
-                            <p>Ciudad: </p>
-                            <p>Comuna: </p>
-                            <p>
-                                <Button bsStyle="default">Agregar</Button>
-                            </p>
-                        </Thumbnail>
-                    </Col>
-                </Row>
-            </Grid>
-        )
-    }
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col xs={6} md={4}>
+            <Thumbnail src="/thumbnaildiv.png" alt="242x200">
+              <h3>Nombre: </h3>
+              <p>Precio: </p>
+              <p>Condicion: </p>
+              <p>Disponibles: </p>
+              <p>Vendidos: </p>
+              <p>Ciudad: </p>
+              <p>Comuna: </p>
+              <p>
+                <Button bsStyle="default">Agregar</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
 
 }
 
